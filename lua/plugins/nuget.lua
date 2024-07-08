e68654e3623454abc2346cb5cc16eba0c0863114
watsonsz <xxmozombiexx@gmail.com>
2024-07-08 15:32:40 -0500

    Nvim Config-Almost Complete
    This is my nvim configuration ~almost~ finished for use in a .NET env.

---
+++ lua/plugins/nuget.lua
@@ -0,0 +1,9 @@
+return
+  {
+    'markwoodhall/vim-nuget',
+    dependencies = {
+      'mattn/webapi-vim',
+      'junegunn/fzf.vim',
+      'Shougo/deoplete.nvim'
+    }
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return	
+  {
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = {"lua", "javascript", "c_sharp", "markdown_inline"},
+        highlight = {enable =true},
+        indent = {enable = true},
+      })  
+    end
+  
+  }
+ 
