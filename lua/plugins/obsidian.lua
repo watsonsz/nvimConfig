727d225380466c5db595a326b07659fa759b1e5e
watsonsz <xxmozombiexx@gmail.com>
2024-07-13 01:24:47 -0500

    updated obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -34,12 +34,12 @@ return {
       },
     },
     -- see below for full list of options 👇
-    templates = {
-      folder = "templates",
-      date_format = "%Y-%m-%d",
-      time_format = "%H:%M",
+    --templates = {
+    --  folder = "templates",
+    --  date_format = "%Y-%m-%d",
+    --  time_format = "%H:%M",
       -- A map for custom variables, the key should be the variable and the value a function
-      substitutions = {},
-    },
+    --  substitutions = {},
+    --},
   },
 }
